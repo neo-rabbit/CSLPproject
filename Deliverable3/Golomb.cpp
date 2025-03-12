da3b308095cd23433b3771c217fe26fa988a5614
neo-rabbit <diogo.m.guedes@ua.pt>
2025-03-12 13:03:55 +0000

    golomb encoding with positive/negative interleaving

--- Deliverable3/Golomb.cpp
+++ Deliverable3/Golomb.cpp
@@ -13,36 +13,44 @@ GolombCoding::GolombCoding(bool il){
 
 vector<bool> GolombCoding::encode(int n, int m){
     vector<bool> res = {};
-    int q = floor(abs(n)/m);
-    int r = abs(n)%m;
+    int N;
     if (interleaving){
+        // set up positive/negative interleaving
+        if (n>=0) {
+            N = n*2;
+        } else {
+            N = n*(-2)-1;
+        }
     } else {
+        N = abs(n);
         // Represent signal by sign and magnitude
         if (n>=0){
             res.push_back(0);
         } else {
             res.push_back(1);
         }
-        // Represnt q by its unary code
-        for(int i=0; i<q; i++){
-            res.push_back(1);
+    }
+    int q = floor(N/m);
+    int r = N%m;
+    // Represnt q by its unary code
+    for(int i=0; i<q; i++){
+        res.push_back(1);
+    }
+    res.push_back(0);
+    // Represent r by its binary code
+    if (m%2 == 0){
+        for (int i = m/2 - 1; i >= 0; i--) {
+            res.push_back((r >> i) & 1);
         }
-        res.push_back(0);
-        // Represent r by its binary code
-        if (m%2 == 0){
-            for (int i = m/2 - 1; i >= 0; i--) {
+    } else {
+        int b = ceil(log2(m));
+        if (r < exp2(b)-m){
+            for (int i = b - 2; i >= 0; i--) {
                 res.push_back((r >> i) & 1);
             }
         } else {
-            int b = ceil(log2(m));
-            if (r < exp2(b)-m){
-                for (int i = b - 2; i >= 0; i--) {
-                    res.push_back((r >> i) & 1);
-                }
-            } else {
-                for (int i = b - 1; i >= 0; i--) {
-                    res.push_back(((r+int(exp2(b))-m) >> i) & 1);
-                }
+            for (int i = b - 1; i >= 0; i--) {
+                res.push_back(((r+int(exp2(b))-m) >> i) & 1);
             }
         }
     }
@@ -54,7 +62,7 @@ int GolombCoding::decode(vector<bool> bits){
 }
 
 int main(int argc, char** argv){
-    GolombCoding golomb(false);
+    GolombCoding golomb(true);
     for(int i = -4; i < 16; i++){
         vector<bool> v = golomb.encode(i, 5);
         for(int j=0; j<v.size(); j++)
